d09f32cdb79559f41984c51ebb81f4617c33cc1c
GorNishanov <GorNishanov@gmail.com>
2016-06-16 06:39:43 -0700

    compiles now

--- lib/Transforms/Coroutines/CoroEarly.cpp
+++ lib/Transforms/Coroutines/CoroEarly.cpp
@@ -18,6 +18,7 @@
 #include "llvm/Pass.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/ADT/SetVector.h"
+#include "llvm/ADT/TinyPtrVector.h"
 #include "llvm/IR/InstIterator.h"
 
 using namespace llvm;
@@ -27,39 +28,69 @@ using namespace llvm;
 namespace {
   /// Holds all structural Coroutine Intrinsics for a particular function
   struct CoroutineShape {
-    CoroInitInst* const CoroInit;
-    CoroAllocInst* CoroAlloc;
-    CoroForkInst* CoroFork = nullptr;
-    CoroFreeInst* CoroFree = nullptr;
-    CoroEndInst* CoroEnd = nullptr;
+    TinyPtrVector<CoroInitInst*> CoroInit;
+    TinyPtrVector<CoroAllocInst*> CoroAlloc;
+    TinyPtrVector<CoroBeginInst*> CoroBegin;
+    TinyPtrVector<CoroEndInst*> CoroEndFinal;
+    TinyPtrVector<CoroEndInst*> CoroEndUnwind;
 
-    CoroutineShape(CoroInitInst*);
+    SmallVector<CoroSizeInst*, 2> CoroSize;
+    SmallVector<CoroFreeInst*, 2> CoroFree;
+    SmallVector<CoroFrameInst*, 4> CoroFrame;
+    SmallVector<CoroSuspendInst*, 4> CoroSuspend;
 
-    explicit operator bool() const {
-      return CoroFork != nullptr && CoroFree != nullptr && CoroEnd != nullptr;
-    }
+    TinyPtrVector<ReturnInst*> Return;
+
+    explicit CoroutineShape(Function &F);
+    explicit CoroutineShape(CoroInitInst *CoroInit)
+        : CoroutineShape(*CoroInit->getParent()->getParent()) {}
   };
 }
 
-CoroutineShape::CoroutineShape(CoroInitInst *CoroInit)
-  : CoroInit(CoroInit), CoroAlloc(CoroInit->getAlloc()) {
-  for (User* U : CoroInit->users()) {
-    if (auto II = dyn_cast<CoroIntrinsic>(U)) {
+CoroutineShape::CoroutineShape(Function &F) {
+  for (Instruction& I : instructions(F)) {
+    if (auto RI = dyn_cast<ReturnInst>(&I))
+      Return.push_back(RI);
+    else if (auto II = dyn_cast<IntrinsicInst>(&I)) {
       switch (II->getIntrinsicID()) {
       default:
         continue;
-      case Intrinsic::coro_fork:
-        CoroFork = cast<CoroForkInst>(II);
+      case Intrinsic::coro_size:
+        CoroSize.push_back(cast<CoroSizeInst>(II));
         break;
-      case Intrinsic::experimental_coro_delete:
-        CoroFree = cast<CoroFreeInst>(II);
+      case Intrinsic::coro_alloc:
+        CoroAlloc.push_back(cast<CoroAllocInst>(II));
+        break;
+      case Intrinsic::coro_init: {
+        auto CI = cast<CoroInitInst>(II);
+        if (CI->isPreSplit())
+          CoroInit.push_back(CI);
+        break;
+      }
+      case Intrinsic::coro_begin:
+        CoroBegin.push_back(cast<CoroBeginInst>(II));
+        break;
+      case Intrinsic::coro_free:
+        CoroFree.push_back(cast<CoroFreeInst>(II));
         break;
       case Intrinsic::coro_end:
-        CoroEnd = cast<CoroEndInst>(II);
+        auto CE = cast<CoroEndInst>(II);
+        if (CE->isFallthrough())
+          CoroEndFinal.push_back(CE);
+        else
+          CoroEndUnwind.push_back(CE);
         break;
       }
     }
   }
+  assert(CoroInit.size() != 1 &&
+         "coroutine should have exactly one defining @llvm.coro.init");
+  assert(CoroBegin.size() != 1 &&
+         "coroutine should have exactly one @llvm.coro.begin");
+  assert(CoroAlloc.size() != 1 &&
+         "coroutine should have exactly one @llvm.coro.alloc");
+  assert(CoroEndFinal.size() != 1 &&
+    "coroutine should have exactly one @llvm.coro.end(falthrough = true)");
 }
 
 static BasicBlock* splitBlockIfNotFirst(Instruction* I, StringRef Name) {
@@ -72,37 +103,22 @@ static BasicBlock* splitBlockIfNotFirst(Instruction* I, StringRef Name) {
   return BB->splitBasicBlock(I, Name);
 }
 
-
+#if 0
 static void outlineAllocPart(CoroutineShape& S, CoroPartExtractor& E) {
   auto Start = splitBlockIfNotFirst(S.CoroAlloc, "AllocPart");
   auto End = splitBlockIfNotFirst(S.CoroInit, "InitBB");
-
-  // after we extract the AllocPart, we no longer need CoroAlloc
-  S.CoroAlloc->replaceAllUsesWith(
-      ConstantPointerNull::get(cast<PointerType>(S.CoroAlloc->getType())));
-  S.CoroAlloc->eraseFromParent();
-  S.CoroAlloc = nullptr;
   E.createFunction(Start, End);
 }
 
 static void outlineFreePart(CoroutineShape& S, CoroPartExtractor& E) {
   auto Start = splitBlockIfNotFirst(S.CoroFree, "FreePart");
   auto End = splitBlockIfNotFirst(S.CoroEnd, "EndBB");
-
-  S.CoroFree->addAttribute(0, Attribute::NonNull);
   E.createFunction(Start, End);
 }
 
 static void outlinePrepPart(CoroutineShape const& S, CoroPartExtractor& E) {
   auto Start = splitBlockIfNotFirst(S.CoroInit->getNextNode(), "PrepPart");
   auto End = splitBlockIfNotFirst(S.CoroFork, "ForkBB");
-  E.createFunction(Start, End);
-}
-
-static void outlineBodyPart(CoroutineShape const& S, CoroPartExtractor& E) {
-  auto Start = splitBlockIfNotFirst(S.CoroFork->getNextNode(), "BodyPart");
-  auto End = splitBlockIfNotFirst(S.CoroEnd, "EndBB");
-  End->eraseFromParent();
   E.createFunction(Start, End);
 }
 
@@ -123,9 +139,6 @@ static void outlineReturnPart(CoroutineShape const& S, CoroPartExtractor& E) {
   auto Start = splitBlockIfNotFirst(S.CoroEnd, "RetPart");
   auto End = splitBlockIfNotFirst(RetInstr, "RetBB");
 
-  // we no longer need the CoroEnd
-  //S.CoroEnd->eraseFromParent();
-
   E.createFunction(Start, End);
 }
 
@@ -140,10 +153,32 @@ static void outlineCoroutineParts(CoroutineShape& S) {
   outlineAllocPart(S, Extractor);
   outlinePrepPart(S, Extractor);
   outlineFreePart(S, Extractor);
-  outlineBodyPart(S, Extractor);
   outlineReturnPart(S, Extractor);
 }
 
+static void replaceEmulatedIntrinsicsWithRealOnes(Module& M) {
+  for (Function& F : M) {
+    for (Instruction &I : instructions(&F)) {
+      if (auto CI = dyn_cast<CallInst>(&I)) {
+        if (auto F = CI->getCalledFunction()) {
+          const auto id = StringSwitch<unsigned>(F->getName())
+            .Case("CoroAlloc", Intrinsic::coro_alloc)
+            .Case("CoroInit", Intrinsic::coro_init)
+            .Case("CoroStart", Intrinsic::coro_start)
+            .Case("CoroFree", Intrinsic::coro_free)
+            .Case("CoroEnd", Intrinsic::coro_end)
+            .Default(0);
+          switch (id) {
+          case Intrinsic::coro_alloc:
+            CoroAllocInst::replaceCall(CI);
+            break;
+          }
+        }
+      }
+    }
+  }
+}
+#endif
 //===----------------------------------------------------------------------===//
 //                              Top Level Driver
 //===----------------------------------------------------------------------===//
@@ -155,32 +190,23 @@ struct CoroEarly : public FunctionPass {
 
   bool doInitialization(Module& M) override {
     SmallVector<CoroInitInst*, 8> Coroutines;
-#if EMULATE_INTRINSICS
-    bool changed = false;
-    for (Function& F : M)
-      for (Instruction& I : instructions(&F))
-        if (auto *CoroInit = dyn_cast<CoroInitInst>(&I))
-          Coroutines.push_back(CoroInit);
-
-    for (CoroInitInst* CI: Coroutines) {
-      if (CoroutineShape S = { CI }) {
-        outlineCoroutineParts(S);
-        changed = true;
-        break;
-      }
-    }
-    return changed;
-#else
+    // replaceEmulatedIntrinsicsWithRealOnes(M);
     Function *CoroInitFn = Intrinsic::getDeclaration(&M, Intrinsic::coro_init);
 
-    // find all coroutines and outline their parts
+    // Find all pre-split coroutines.
     for (User* U : CoroInitFn->users())
       if (auto CoroInit = dyn_cast<CoroInitInst>(U))
-        if (CoroutineShape S = { CoroInit })
-          outlineCoroutineParts(S);
+        if (CoroInit->isPreSplit())
+          Coroutines.push_back(CoroInit);
 
-    return !CoroInitFn->user_empty();
-#endif
+    // Outline coroutine parts to guard against code movement
+    // during optimizations. We inline them back in CoroSplit.
+    for (CoroInitInst *CI : Coroutines) {
+      CoroutineShape S{ CI }; // TODO: move out
+      //outlineCoroutineParts(S);
+    }
+
+    return !Coroutines.empty();
   }
 
   bool runOnFunction(Function &F) override {
--- lib/Transforms/Coroutines/CoroutineCommon.h
+++ lib/Transforms/Coroutines/CoroutineCommon.h
@@ -15,13 +15,17 @@
 #ifndef LLVM_LIB_TRANSFORMS_COROUTINES_COROUTINECOMMON_H
 #define LLVM_LIB_TRANSFORMS_COROUTINES_COROUTINECOMMON_H
 
-#include "llvm/Transforms/Coroutines.h"
-#include <llvm/IR/IntrinsicInst.h>
-#include "llvm/ADT/SetVector.h"
-#include "llvm/ADT/StringSwitch.h"
+#include "CoroInstr.h"
+
+#include <llvm/Transforms/Coroutines.h>
+#include <llvm/ADT/SetVector.h>
 #include <llvm/PassRegistry.h>
 
 namespace llvm {
+
+class Function;
+class BasicBlock;
+class Constant;
 
 struct LLVM_LIBRARY_VISIBILITY CoroPartExtractor {
   Function *createFunction(BasicBlock *Start, BasicBlock *End);
@@ -39,151 +43,6 @@ struct LLVM_LIBRARY_VISIBILITY CoroCommon {
 }
 
 namespace llvm {
-
-#define EMULATE_INTRINSICS 1
-
-#if EMULATE_INTRINSICS // emulate intrinsics via functions for faster turnaround
-
-  class LLVM_LIBRARY_VISIBILITY CoroIntrinsic : public CallInst {
-  public:
-    // Methods to support type inquiry through isa, cast, and dyn_cast:
-    static inline bool classof(const CallInst *I) {
-      return I->getCalledFunction() != nullptr;
-    }
-    static inline bool classof(const Value *V) {
-      return isa<CallInst>(V) && classof(cast<CallInst>(V));
-    }
-    int getIntrinsicID() const {
-      return StringSwitch<int>(getCalledFunction()->getName())
-        .Case("CoroAlloc", Intrinsic::experimental_coro_elide)
-        .Case("CoroInit", Intrinsic::coro_init)
-        .Case("CoroStart", Intrinsic::coro_fork)
-        .Case("CoroFree", Intrinsic::experimental_coro_delete)
-        .Case("CoroEnd", Intrinsic::coro_end)
-        .Default(0);
-
-    }
-  };
-
-  class LLVM_LIBRARY_VISIBILITY CoroEndInst : public CoroIntrinsic {
-  public:
-    // Methods to support type inquiry through isa, cast, and dyn_cast:
-    static inline bool classof(const CoroIntrinsic *I) {
-      return I->getIntrinsicID() == Intrinsic::coro_end;
-    }
-    static inline bool classof(const Value *V) {
-      return isa<CoroIntrinsic>(V) && classof(cast<CoroIntrinsic>(V));
-    }
-  };
-
-  class LLVM_LIBRARY_VISIBILITY CoroAllocInst : public CoroIntrinsic {
-  public:
-    // Methods to support type inquiry through isa, cast, and dyn_cast:
-    static inline bool classof(const CoroIntrinsic *I) {
-      return I->getIntrinsicID() == Intrinsic::experimental_coro_elide;
-    }
-    static inline bool classof(const Value *V) {
-      return isa<CoroIntrinsic>(V) && classof(cast<CoroIntrinsic>(V));
-    }
-  };
-
-  class LLVM_LIBRARY_VISIBILITY CoroForkInst : public CoroIntrinsic {
-  public:
-    // Methods to support type inquiry through isa, cast, and dyn_cast:
-    static inline bool classof(const CoroIntrinsic *I) {
-      return I->getIntrinsicID() == Intrinsic::coro_fork;
-    }
-    static inline bool classof(const Value *V) {
-      return isa<CoroIntrinsic>(V) && classof(cast<CoroIntrinsic>(V));
-    }
-  };
-
-  class LLVM_LIBRARY_VISIBILITY CoroFreeInst : public CoroIntrinsic {
-  public:
-    // Methods to support type inquiry through isa, cast, and dyn_cast:
-    static inline bool classof(const CoroIntrinsic *I) {
-      return I->getIntrinsicID() == Intrinsic::experimental_coro_delete;
-    }
-    static inline bool classof(const Value *V) {
-      return isa<CoroIntrinsic>(V) && classof(cast<CoroIntrinsic>(V));
-    }
-  };
-
-  class LLVM_LIBRARY_VISIBILITY CoroInitInst : public CoroIntrinsic {
-    enum { kMem, kElide, kAlign, kPromise, kMeta };
-  public:
-    Value *getMem() const {
-      return getArgOperand(kMem);
-    }
-    CoroAllocInst *getAlloc() const {
-      return dyn_cast<CoroAllocInst>(getArgOperand(kElide));
-    }
-    bool isPostSplit() const { return false; }
-
-    Function *getCoroutine() const {
-      return const_cast<Function *>(getParent()->getParent());
-    }
-
-    // Methods to support type inquiry through isa, cast, and dyn_cast:
-    static inline bool classof(const CoroIntrinsic *I) {
-      return I->getIntrinsicID() == Intrinsic::coro_init;
-    }
-    static inline bool classof(const Value *V) {
-      return isa<CoroIntrinsic>(V) && classof(cast<CoroIntrinsic>(V));
-    }
-  };
-
-#else
-/// This represents the llvm.coro.elide instruction.
-class LLVM_LIBRARY_VISIBILITY CoroElideInst : public IntrinsicInst {
-public:
-  // Methods to support type inquiry through isa, cast, and dyn_cast:
-  static inline bool classof(const IntrinsicInst *I) {
-    return I->getIntrinsicID() == Intrinsic::experimental_coro_elide;
-  }
-  static inline bool classof(const Value *V) {
-    return isa<IntrinsicInst>(V) && classof(cast<IntrinsicInst>(V));
-  }
-};
-
-/// This represents the llvm.coro.init instruction.
-class LLVM_LIBRARY_VISIBILITY CoroInitInst : public IntrinsicInst {
-  enum { kMem, kElide, kAlign, kPromise, kMeta };
-public:
-  CoroElideInst *getElide() const {
-    return dyn_cast<CoroElideInst>(getArgOperand(kElide));
-  }
-
-  Value *getMem() const { return getArgOperand(kMem); }
-
-  ConstantInt *getAlignment() const {
-    return cast<ConstantInt>(getArgOperand(kAlign));
-  }
-
-  Function *getCoroutine() const {
-    auto MD = cast<MetadataAsValue>(getArgOperand(kMeta))->getMetadata();
-    auto MV = dyn_cast<ValueAsMetadata>(MD);
-    if (!MV)
-      return nullptr;
-
-    auto F = dyn_cast<Function>(MV->getValue());
-    assert(getParent()->getParent() == F &&
-           "coro.init metadata does not point at enclosing function");
-    return F;
-  }
-
-  bool isPostSplit() const { return getCoroutine() == nullptr; }
-
-  // Methods for support type inquiry through isa, cast, and dyn_cast:
-  static inline bool classof(const IntrinsicInst *I) {
-    return I->getIntrinsicID() == Intrinsic::coro_init;
-  }
-  static inline bool classof(const Value *V) {
-    return isa<IntrinsicInst>(V) && classof(cast<IntrinsicInst>(V));
-  }
-};
-#endif
-
 void initializeCoroEarlyPass(PassRegistry &Registry);
 void initializeCoroElidePass(PassRegistry &Registry);
 void initializeCoroCleanupPass(PassRegistry &registry);
